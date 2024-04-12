3b876d85feeae5170af01709773a433d0527c209
Lalith Viswas <126174983+lalithviswas@users.noreply.github.com>
2024-04-12 22:57:05 +0530

    Add files via upload

---
+++ Java Full Stack Development/Java_Practice/Spring/IoC/src/main/java/com/example/demo/Fresher.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Fresher {
+    public void giveInterview(){
+        System.out.println("InterviewDone");
+    }
+    public void getSkilled(){
+        System.out.println("SkilledUp");
+    }
+}
